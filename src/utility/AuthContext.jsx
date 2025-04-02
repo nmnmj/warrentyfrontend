6d5f03e4938b5490f8d3e99421e9d6dd5740cf94
naman-techwink <naman.jay@techwink.net>
2025-04-02 15:52:06 +0530

    second

--- src/utility/AuthContext.jsx
+++ src/utility/AuthContext.jsx
@@ -5,7 +5,11 @@ import { showToast } from "./Toast";
 const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(() => {
+    // Load user from localStorage if available
+    const storedUser = localStorage.getItem("user");
+    return storedUser ? JSON.parse(storedUser) : null;
+  });
 
   // Fetch user data from backend
   const fetchUser = async () => {
